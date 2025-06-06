f7c4af3826cfb96f625c5ff1a9d306f0d35b0a67
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-06-06 19:32:12 +0800

    added hash generator

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		cssCodeSplit: false,
+		rollupOptions: {
+			output: {
+				inlineDynamicImports: true,
+				manualChunks: undefined
+			}
+		}
+	},
+	css: {
+		// This will inline all CSS
+		extract: false
+	}
 });
